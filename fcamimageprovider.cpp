5aa07c22c021b65af53bd1d59ab8313f769a0005
Lev Zelenskiy <lev.zelensky@gmail.com>
2012-06-04 20:49:36 +1000

    FCam image provider changes - reinitialize camera every iteration; - save JPEG with 100 quality;

--- fcamimageprovider.cpp
+++ fcamimageprovider.cpp
@@ -9,59 +9,57 @@ FCamImageProvider::FCamImageProvider(QObject *parent) :
 
 bool FCamImageProvider::init()
 {
-    // Attach the lens to the sensor
-    m_sensor.attach(&m_lens);
-
-    qDebug() << "Near focus:" << m_lens.nearFocus()
-             << "Far focus:" << m_lens.farFocus();
-
-    // Explicitly power up the sensor
-    if (m_sensor.initialize(0) == -1){
-        qDebug() << "Error powering up the sensor.";
-        return 1;
-    }
-
-    // Focus to 1D (1m) with maximal speed
-    m_lens.setFocus(1.f, m_lens.maxFocusSpeed());
-    qDebug() << "Changing focus";
-    while (m_lens.focusChanging()) { } // Wait to be done
-    qDebug() << "Focus changed";
-
-    m_shot.exposure = 40000; // 40 ms exposure
-    m_shot.gain = 1.0f;      // minimum ISO
-
-    // Specify the output resolution and format, and allocate storage for the resulting image
-    m_shot.image = FCam::Image(2304, 1296, FCam::UYVY);
-
     return true;
 }
 
 void FCamImageProvider::requestNextImage()
 {
+    FCam::N9::Sensor sensor;
+    FCam::Shot shot;
+    FCam::N9::Lens lens;
+
+    // Attach the lens to the sensor
+    sensor.attach(&lens);
+
+    // Explicitly power up the sensor
+    if (sensor.initialize(0) == -1){
+        qDebug() << "Error powering up the sensor.";
+        return;
+    }
+
+    // Focus to 1D (1m) with maximal speed
+    lens.setFocus(1.f, lens.maxFocusSpeed());
+    while (lens.focusChanging()) { } // Wait to be done
+
+    shot.exposure = 40000; // 40 ms exposure
+    shot.gain = 1.0f;      // minimum ISO
+
+    // Specify the output resolution and format, and allocate storage for the resulting image
+    shot.image = FCam::Image(2304, 1296, FCam::UYVY);
+
     // Order the sensor to capture a shot
-    m_sensor.capture(m_shot);
+    sensor.capture(shot);
     errorCheck();
-    Q_ASSERT(m_sensor.shotsPending() == 1); // There should be exactly one shot
+    Q_ASSERT(sensor.shotsPending() == 1); // There should be exactly one shot
 
     // Retrieve the frame from the sensor
-    FCam::Frame frame = m_sensor.getFrame();
-    Q_ASSERT(frame.shot().id == m_shot.id); // This frame should be the result of the shot we made
+    FCam::Frame frame = sensor.getFrame();
+    Q_ASSERT(frame.shot().id == shot.id); // This frame should be the result of the shot we made
 
     // This frame should be valid too
     Q_ASSERT(frame.valid());
     Q_ASSERT(frame.image().valid());
 
-    qDebug() << "Image Format" << frame.image().type();
-
-    // Save the frame
+    // TODO: Convert from FCam::Frame to QImage directly
+    // Save the frame with maximum JPG quality
     char path[256];
     snprintf(path, 256, "/home/user/fcam%03d.jpg", m_index);
-    FCam::saveJPEG(frame, path);
+    FCam::saveJPEG(frame, path, 100);
     m_index++;
 
     // Check that the pipeline is empty
-    Q_ASSERT(m_sensor.framesPending() == 0);
-    Q_ASSERT(m_sensor.shotsPending() == 0);
+    Q_ASSERT(sensor.framesPending() == 0);
+    Q_ASSERT(sensor.shotsPending() == 0);
 
     QImage image(path);
     if (image.isNull()) {
--- fcamimageprovider.h
+++ fcamimageprovider.h
@@ -22,9 +22,6 @@ public slots:
 private:
     void errorCheck();
 
-    FCam::N9::Sensor m_sensor;
-    FCam::Shot m_shot;
-    FCam::N9::Lens m_lens;
     int m_index;
 };
 
